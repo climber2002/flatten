284f1e244697f94e860446bcac5e4a9afacc8bee
Andy Wang <climber2002@gmail.com>
2017-11-12 15:50:04 +1100

    Finish part14

--- basics/src/main/scala/flatten/Part14.scala
+++ basics/src/main/scala/flatten/Part14.scala
@@ -1,6 +1,10 @@
 package flatten
 
+import scala.concurrent.Future
+import scalaz.OptionT
 import scalaz.Scalaz._
+import scalaz._
+import scala.concurrent.ExecutionContext.Implicits.global
 
 trait Part14 extends Part13 {
 
@@ -21,4 +25,12 @@ trait Part14 extends Part13 {
 
 
   def double(i: Int) = i * 2
+
+  for {
+    username <- getUserName(data) |> Future.successful |> OptionT.apply
+    user <- getUser(username) |> OptionT.apply
+    email = getEmail(user)
+    validatedEmail <- validateEmail(email) |> Future.successful |> OptionT.apply
+    success <- sendEmail(email) |> OptionT.apply
+  } yield success
 }
